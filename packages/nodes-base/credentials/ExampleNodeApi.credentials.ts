bce1687b78c04dfcc1f7e7045e63cec6fb09f7a5
your_username <pad128g@icloud.com>
2023-11-15 11:37:42 -0500

    update Example Credental

---
+++ packages/nodes-base/credentials/ExampleNodeApi.credentials.ts
@@ -0,0 +1,48 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ExampleNodeApi implements ICredentialType {
+	name = 'exampleNodeApi';
+
+	displayName = 'Example Node API';
+
+	documentationUrl = '';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'X-RapidAPI-Key',
+			name: 'xRapidApiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+		{
+			displayName: 'X-RapidAPI-Host',
+			name: 'xRapidApiHost',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'X-RapidAPI-Key': '={{$credentials.xRapidApiKey}}',
+				'X-RapidAPI-Host': '={{$credentials.xRapidApiHost}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			url: 'https://odds.p.rapidapi.com/v4/sports',
+			method: 'GET',
+		},
+	};
+}
